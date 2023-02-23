0944101ee2a5984d94232961d71674f26f1365cc
Oleg Danko <olegdanko@outlook.com>
2023-02-23 02:06:05 +0200

    Minor fixes

--- src/GameState/GameStateClient.cpp
+++ src/GameState/GameStateClient.cpp
@@ -1,6 +1,8 @@
 #include <GameState/GameStateClient.hpp>
 
+#include <atomic>
+
 size_t get_next_game_object_id() {
     static std::atomic_size_t id{1};
-    return id.fetch_and_add(1);
+    return id.fetch_add(1);
 }
